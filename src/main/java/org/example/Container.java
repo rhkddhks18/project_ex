35c0bded2604b15e29a6666eb6571fcf6f2405cc
hwjk16322 <hwjk16322@gmail.com>
2023-10-31 11:07:05 +0900

    dbconnection container

--- src/main/java/org/example/Container.java
+++ src/main/java/org/example/Container.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import org.example.db.DBConnection;
+
 import java.util.Scanner;
 
 public class Container {
     private static Scanner sc;
+    private static DBConnection dbConnection;
 
     public static void init() {
         sc = new Scanner(System.in);
@@ -13,5 +16,12 @@ public class Container {
     }
     public static void close(){
         sc.close();
+    }
+
+    public static DBConnection getDBconnection() {
+        if (dbConnection == null) {
+            dbConnection = new DBConnection();
+        }
+        return dbConnection;
     }
 }
