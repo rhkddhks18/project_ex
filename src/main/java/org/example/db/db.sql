e09e73a702d620bb39cb82464552a59d6b61ae7f
깃rhkddhks18 <taengja18@naver.com>
2023-11-08 04:34:17 +0900

    수정중

--- src/main/java/org/example/db/db.sql
+++ src/main/java/org/example/db/db.sql
@@ -10,6 +10,9 @@ CREATE TABLE `user` (
 	email varchar(100) NOT NULL unique
 );
 
+SELECT * FROM `user`;
+
+DROP table movie;
 CREATE TABLE movie (
 	id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	title varchar(100) NOT NULL,
@@ -18,25 +21,26 @@ CREATE TABLE movie (
 	genre varchar(30) NOT NULL
 );
 
-
+SELECT * FROM movie;
+DROP table schedule;
 CREATE TABLE schedule (
 	id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	movie_id int not null,
 	movie_time varchar(30) NOT NULL
 );
 
-
-CREATE TABLE seat (
-	id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	seat_x varchar(3) NOT NULL,
-	seat_y varchar(3) NOT NULL
-);
+SELECT * FROM schedule;
 
 
 CREATE TABLE movie_reservation (
 	id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	is_complete char(30) NOT NULL
+	user_id int NOT null,
+	schedule_id int not null,
+	seat_x int NOT NULL,
+	seat_y int NOT NULL
 );
 
+SELECT * FROM movie_reservation;
 
 CREATE TABLE review (
 	id int UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -45,6 +49,10 @@ CREATE TABLE review (
 	user_id varchar(30) NOT NULL,
 	regDate varchar(30) not null
 );
+
+SELECT * FROM review;
+
+
 
 
 INSERT INTO `movie`
@@ -70,19 +78,66 @@ director = "피터 스피어리그, 마이클 스피어리그",
 actor = "에단 호크, 사라 스누크, 노아 테일러 외",
 genre = "SF";
 
+SELECT * FROM `movie`;
+
+
+SELECT * FROM SCHEDULE ;
+
 
 INSERT INTO schedule
 set
+movie_id = 0,
 movie_time = "1:00 PM";
 
 INSERT INTO schedule
 set
+movie_id = 0,
 movie_time = "4:00 PM";
 
 INSERT INTO schedule
 set
+movie_id = 0,
 movie_time = "7:00 PM";
 
 INSERT INTO schedule
 set
+movie_id = 0,
 movie_time = "10:00 PM";
+select * from schedule;
+
+select *
+from schedule
+join movie on schedule.movie_id = movie.id;
+
+INSERT INTO schedule (movie_id, movie_time)
+SELECT id, '1:00 PM' FROM movie WHERE id = 2;
+
+SELECT S.*
+  FROM schedule AS S
+  INNER JOIN movie AS M
+  ON S.movie_id = M.id
+ Where M.id = 1;
+
+select * from movie;
+
+select *
+  from movie_reservation
+ where schedule_id ;
+
+ insert into movie_reservation(user_id, schedule_id, seat_x, seat_y)
+ select '123', 7, 1, 6
+
+insert into schedule (movie_id, movie_time)
+VALUES(1, '4:00 PM'), (1, '7:00 PM'), (1, '10:00 PM')
+
+insert into `user`
+set id = 1,
+user_id = "123",
+`password` = "123",
+name = "고",
+birth = "97.12.08",
+email = "rhkddhks18@gmail.com";
+
+select * from schedule where id = 1;
+
+select * from schedule;
