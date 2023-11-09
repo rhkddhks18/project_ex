111c28cf80736c5e27daf2c1a99a116c332f6e1c
hwjk16322 <hwjk16322@gmail.com>
2023-11-09 13:12:48 +0900

    123

--- src/main/java/org/example/ticketing/ticketingRepository/MovieReservationRepository.java
+++ src/main/java/org/example/ticketing/ticketingRepository/MovieReservationRepository.java
@@ -18,7 +18,7 @@ public class MovieReservationRepository {
 //        sb.append(String.format("seat_x = '%s',", seatX));
 //
 //        Container.getDBconnection().insert(sb.toString());
-//    }1123123123dd
+//    }1123123123dd123g
 
     public List<MovieReservation> getReservatedSeat(Schedule time) {
         StringBuilder sb = new StringBuilder();
